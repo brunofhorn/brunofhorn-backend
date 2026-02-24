7b9ad9e44d02aab97d6c8019a5a20f4e4998390c
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2026-02-24 18:54:38 -0300

    feat: change database pool

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,25 +1,15 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
 
 declare global {
   // eslint-disable-next-line no-var
   var __prisma: PrismaClient | undefined;
 }
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
+if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not configured.");
 }
 
-const pool = new Pool({
-  connectionString,
-});
-
-const adapter = new PrismaPg(pool);
-
-export const prisma = globalThis.__prisma ?? new PrismaClient({ adapter });
+export const prisma = globalThis.__prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.__prisma = prisma;
